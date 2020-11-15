0704d3ec46363763f17cf927efca10d456f6c401
EC2 Default User <ec2-user@ip-172-31-73-127.ec2.internal>
2020-11-15 17:29:54 +0000

    adding index file

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     
     <head>
         <meta charset="utf-8"/>
-        <title>The Local Weather Update For Your City</title>
+        <title>The Local Weather Update For Your City!</title>
         
         <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
         <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
